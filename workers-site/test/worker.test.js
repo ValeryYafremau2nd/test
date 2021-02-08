237f6f54204ca4cffc303df9020c72973187788f
ValeryYafremau2nd <dezmand_palmus_germany@mail.ru>
2021-02-08 12:26:00 +0300

    actions

---
+++ workers-site/test/worker.test.js
@@ -0,0 +1,21 @@
+const Cloudworker = require('@dollarshaveclub/cloudworker');
+const fs = require('fs')
+const path = require('path')
+
+const { expect } = require('chai');
+
+const workerScript = fs.readFileSync(
+  path.resolve(__dirname, '../../dist/worker.js'),
+  'utf8',
+);
+
+describe('unit test', function () {
+  before(() => {
+    worker = new Cloudworker(workerScript);
+  });
+  it('get /api/test', async () => {
+    const req = new Cloudworker.Request('https://test/api/test');
+    const res = await worker.dispatch(req);
+    expect(res.status).to.eql(200);
+  });
+});
