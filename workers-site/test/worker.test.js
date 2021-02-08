95a141d2289d18dfc34cfc19c3f7410d7cb6b495
ValeryYafremau2nd <dezmand_palmus_germany@mail.ru>
2021-02-08 12:37:39 +0300

    prettier

--- workers-site/test/worker.test.js
+++ workers-site/test/worker.test.js
@@ -1,20 +1,20 @@
-const Cloudworker = require('@dollarshaveclub/cloudworker');
-const fs = require('fs')
-const path = require('path')
+const Cloudworker = require("@dollarshaveclub/cloudworker");
+const fs = require("fs");
+const path = require("path");
 
-const { expect } = require('chai');
+const { expect } = require("chai");
 
 const workerScript = fs.readFileSync(
-  path.resolve(__dirname, '../../dist/worker.js'),
-  'utf8',
+  path.resolve(__dirname, "../../dist/worker.js"),
+  "utf8"
 );
 
-describe('unit test', function () {
+describe("unit test", function () {
   before(() => {
     worker = new Cloudworker(workerScript);
   });
-  it('get /api/test', async () => {
-    const req = new Cloudworker.Request('https://test/api/test');
+  it("get /api/test", async () => {
+    const req = new Cloudworker.Request("https://test/api/test");
     const res = await worker.dispatch(req);
     expect(res.status).to.eql(200);
   });
